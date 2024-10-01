cca011d3c052aa617ed07189fcba274940451083
Luis Rangel <luismrangel7@gmail.com>
2024-10-01 11:35:35 -0500

    subscription middleware and paypal webhook working together

--- server/lambdas/middleware/subscriptionMiddleware.js
+++ server/lambdas/middleware/subscriptionMiddleware.js
@@ -7,20 +7,30 @@ const verifySubscriptionMiddleware = async (req, res, next) => {
         const category = req.params.category;
         const allowedBasicSubCategories = ['punish-game', 'neutral-game', 'edgeguard-game'];
 
+        console.log('userId: ', userId);
+        console.log(typeof userId);
+        console.log('category: ', category);
+
+        if (category === 'introduction') {
+            return next();
+        }
+
         const subscriptionDetails = await getSubscriptionDetails(userId);
         if (!subscriptionDetails) {
             return res.status(403).json({ message: 'Unauthorized access. No subscription found.' });
         }
 
         const { subscriptionStatus, subscriptionTier, subscriptionType, expirationDate } = subscriptionDetails;
-        const expirationDateObj = new Date(expirationDate);
-        const currentDate = new Date();
+        // const expirationDateObj = new Date(expirationDate);
+        // const currentDate = new Date();
 
-        if (subscriptionStatus === 'CANCELLED' && currentDate < expirationDateObj) {
+        const isExpired = checkExpiration(expirationDate);        
+
+        if (subscriptionStatus === 'CANCELLED' && !isExpired) {
             return next();
-        }
+        }   
 
-        if (subscriptionStatus === 'CANCELLED' && currentDate > expirationDateObj) {
+        if (subscriptionStatus === 'CANCELLED' && isExpired) {
             await updateSubscriptionStatus(userId, 'EXPIRED');
             return res.status(403).json({ message: 'Unauthorized access. Subscription expired.' });
         }
@@ -29,13 +39,11 @@ const verifySubscriptionMiddleware = async (req, res, next) => {
             return res.status(403).json({ message: 'Unauthorized access. Subscription expired or suspended.' });
         }
 
-        if (subscriptionStatus === 'ACTIVE' && subscriptionType === 'ONE-TIME' && currentDate > expirationDateObj) {
+        // Keep this commented out for now in case its needed later
+        // if (subscriptionStatus === 'ACTIVE' && subscriptionType === 'ONE-TIME' && isExpired) {
+        if (subscriptionStatus === 'ACTIVE' && isExpired) {
             await updateSubscriptionStatus(userId, 'EXPIRED');
-            return res.status(403).json({ message: 'Unauthorized access. One-time subscription expired.' });
-        }
-
-        if (category === 'introduction') {
-            return next();
+            return res.status(403).json({ message: 'Unauthorized access. Subscription expired. Check your invoices to learn more.' });
         }
 
         if (subscriptionTier === 'Guest') {
@@ -53,16 +61,35 @@ const verifySubscriptionMiddleware = async (req, res, next) => {
     }
 };
 
+// Check if subscription has expired
+// Giving a grace period of 1 day after expiration for paypal to process the payment before updating status to EXPIRED or CANCELLED
+const checkExpiration = (exp) => {
+    const currentDate = new Date();
+    const [year2, month2, day2] = exp.split('-').map(Number);
+    let expirationDate = new Date(year2, month2 - 1, day2);
+    expirationDate.setDate(expirationDate.getDate() + 1);
+    console.log(currentDate, expirationDate);
+    return currentDate > expirationDate;
+}
+
 // Fetch subscription details from DynamoDB
 const getSubscriptionDetails = async (userId) => {
     const params = {
         TableName: 'SSBMF_UserSubscriptions',
-        Key: { userId },
+        FilterExpression: 'userId = :userId',
+        ExpressionAttributeValues: {
+            ':userId': userId
+        }
     };
 
+    console.log("Checking subscription details for user: ", userId);
+
     try {
-        const result = await dynamoDb.get(params).promise();
-        return result.Item;
+        const result = await dynamoDb.scan(params).promise();
+        if (result.Items.length === 0) {
+            throw new Error(`No subscription found for user ${userId}.`);
+        }
+        return result.Items[0];
     } catch (error) {
         console.error('Error fetching subscription details:', error);
         throw new Error('Failed to retrieve subscription data.');
@@ -73,9 +100,13 @@ const getSubscriptionDetails = async (userId) => {
 const updateSubscriptionStatus = async (userId, newStatus) => {
     const params = {
         TableName: 'SSBMF_UserSubscriptions',
-        Key: { userId },
-        UpdateExpression: 'set subscriptionStatus = :s',
-        ExpressionAttributeValues: { ':s': newStatus },
+        Key: { 
+            userId: userId 
+        },
+        UpdateExpression: 'SET subscriptionStatus = :s',
+        ExpressionAttributeValues: { 
+            ':s': newStatus 
+        },
     };
 
     try {
